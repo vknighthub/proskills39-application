c5350745a10cfe44bcd2ea71f5eba4df583a6689
nguyenvuhoang <102274002+vknighthub@users.noreply.github.com>
2023-02-10 23:55:11 +0700

    commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,5 +7,6 @@ export default NextAuth({
             clientId: "1025777977822-169v8ffm9vi1jcn8b7hlbiq34l7qjd8t.apps.googleusercontent.com",
             clientSecret: "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"
         })
-    ]
+    ],
+    secret: process.env.NEXTAUTH_SECRET
 });
