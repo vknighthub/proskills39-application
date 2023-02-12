e46244218da56c2b346b7fa55e3fd36e8c706845
nguyenvuhoang <102274002+vknighthub@users.noreply.github.com>
2023-02-12 22:55:19 +0700

    Update index.jsx

--- src/layouts/info/Category/index.jsx
+++ src/layouts/info/Category/index.jsx
@@ -17,9 +17,6 @@ const Category = ({ data }) => {
             <CategoryPopular data={category} />
             <CategoryExplore data={category} />
             <CategoryFAQs data={category} />
-
-
-
         </>
     )
 }
