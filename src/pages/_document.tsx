a4a22b2f7a0f5cacb003fd2129e5b33613d7c629
nguyenvuhoang <102274002+vknighthub@users.noreply.github.com>
2023-04-23 22:53:08 +0700

    Create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="zalo-platform-site-verification" content="VSwL9V__67qkoifGq_b4RpdfnMxLbnSoDZO" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
