f721c74e373bbae05b059753f5eb83e50cc97f24
hoangnv <nguyenvuhoangz@gmail.com>
2023-06-29 16:45:26 +0700

    Update Categories.jsx

--- src/layouts/main/Categories.jsx
+++ src/layouts/main/Categories.jsx
@@ -1,20 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 const Categories = ({ data }) => {
-
     return (
         data.map((categories, index) => (
             <div className="component-popular-categories" key={index}>
-                <div className="overlap-group-wrapper">
-                    <div className="overlap-group">
-                        <div className="element">
-                            <Image className="img" alt="Element" src={categories.image} width={233} height={114} />
-                            <div className="digital-marketing">{categories.name}</div>
+                <Link href={`/categories/${categories.slug}`}>
+                    <div className="overlap-group-wrapper">
+                        <div className="overlap-group">
+                            <div className="element">
+                                <Image className="img" alt="Element" src={categories.image} width={233} height={114} />
+                                <div className="digital-marketing">{categories.name}</div>
+                            </div>
+                            <p className="text-wrapper">
+                                {categories.details}
+                            </p>
                         </div>
-                        <p className="text-wrapper">
-                            {categories.details}
-                        </p>
                     </div>
-                </div>
+                </Link>
             </div>
         ))
 
