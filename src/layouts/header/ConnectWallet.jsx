9008c39397b077e9380b63de9ef1f50a2a2f1120
hoangnv <nguyenvuhoangz@gmail.com>
2023-07-10 18:57:15 +0700

    Update ConnectWallet.jsx

--- src/layouts/header/ConnectWallet.jsx
+++ src/layouts/header/ConnectWallet.jsx
@@ -1,17 +1,33 @@
-import { CardanoWallet } from "@meshsdk/react";
+import { CardanoWallet, useWallet } from "@meshsdk/react";
 import { useEffect, useState } from "react";
 
 const ConnectWallet = ({ isAuthorized }) => {
     const [render, setRender] = useState(false);
+    const { connect, name, connected} = useWallet();
+    
+    useEffect(() => {
+        var connectWallet = localStorage.getItem('walletname')
+        if (connectWallet) {
+            connect(connectWallet)
+        }
+    }, [])
+
     useEffect(() => {
         setRender(true)
     }, [render])
+
+
+    const handleConnectWallet = (walletname) => {
+        localStorage.setItem('walletname', walletname)
+    }
 
     return (
         <>
             {isAuthorized && render &&
                 <div className="collection-box">
-                    <CardanoWallet />
+                    <CardanoWallet
+                        onConnected={() => handleConnectWallet(name)}
+                    />
                 </div>
             }
         </>
