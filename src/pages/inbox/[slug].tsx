908851ad3a5ba020e8f968b8aefa6b86835ad0c8
hoangnv <nguyenvuhoangz@gmail.com>
2023-07-14 22:26:39 +0700

    commir chat

--- src/pages/inbox/[slug].tsx
+++ src/pages/inbox/[slug].tsx
@@ -1,5 +1,8 @@
+import ChatContent from '@/layouts/info/Chat/ChatContent';
+import ChatSidebarLeft from '@/layouts/info/Chat/ChatSidebarLeft';
 import Layout from '@/layouts/_layout';
 import { NextPageWithLayout } from '@/types';
+import { Box } from '@mui/material';
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react'
@@ -7,9 +10,35 @@ import invariant from 'tiny-invariant';
 
 const InboxPage: NextPageWithLayout<
     InferGetStaticPropsType<typeof getStaticProps>
-> = ({seller}) => {
+> = ({ seller }) => {
     return (
-        <div>{seller}</div>
+        <div className="primary-content-area background-content">
+            <div className="container section-padding">
+                <div className="section-title-wrapper">
+                    <div className="section-title">
+                        Contact to <span className="gradient-text">  {seller}</span>
+                    </div>
+                </div>
+                <div className="featured-box">
+                    <Box
+                        className='app-chat'
+                        sx={{
+                            width: '100%',
+                            display: 'flex',
+                            borderRadius: 1,
+                            overflow: 'hidden',
+                            position: 'relative',
+                            boxShadow: 6,
+                            border: `1px solid #fff`
+                        }}
+                    >
+                        <ChatSidebarLeft />
+                        <ChatContent />
+                    </Box>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
