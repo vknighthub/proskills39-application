f6b7e1155aa392bb5c3670789d676c9f98e9fc33
hoangnv <nguyenvuhoangz@gmail.com>
2023-07-26 11:53:15 +0700

    Update facebookmsg.tsx

--- src/components/plugin/facebookmsg.tsx
+++ src/components/plugin/facebookmsg.tsx
@@ -1,14 +1,22 @@
 'use-client'
 
+import { useRouter } from 'next/router'
 import React from 'react'
 import { FacebookProvider, CustomChat } from 'react-facebook'
 
 const FacebookMsg = () => {
+    const { locale } = useRouter()
     return (
-        <FacebookProvider appId='670951515077559' chatSupport>
+        <FacebookProvider
+            appId='670951515077559'
+            chatSupport
+            language={locale}
+            lazy={true}
+        >
             <CustomChat
                 pageId='100345429819665'
-                minimized={false}
+                minimized={true}
+
             />
         </FacebookProvider>
     )
