9e6c94aab472b52b6a736970d24a9d801d55a8af
Nguyễn Thị Thanh Tâm <87571082+tamn2101@users.noreply.github.com>
2023-09-10 21:16:20 +0700

    Revert "update_translation"
    This reverts commit 07bd050792f21f1dd795b6ab42bbdd1f786244bf.

--- src/components/services/popular-categories.tsx
+++ src/components/services/popular-categories.tsx
@@ -2,19 +2,24 @@ import { Popuparcategory } from '@/types'
 import GridCategories from './popularcategories/GridCategories'
 import { useMediaQuery } from 'react-responsive'
 import SwipperCategories from './popularcategories/SwipperCategories'
+import { useTranslation } from 'next-i18next'
+
 
 type PageProps = {
     data: Popuparcategory[]
 }
 
 export const HomePopularCategory = ({ data }: PageProps) => {
+    const { t } = useTranslation('common')
+
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
 
     return (
         <div className="container section-padding">
             <div className="section-title-wrapper">
                 <div className="section-title">
-                    Popular<span className="gradient-text"> Categories</span>
+                    {t('text-categories')}<span className="gradient-text">  {t('text-popular')}</span>
+                    
                 </div>
             </div>
             {isTabletOrMobile
